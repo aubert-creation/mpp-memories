2392230d3a30408e723d011e73b0a157467a2991
Louis A <louis.aubert@monpetitgazon.com>
2022-11-20 15:16:27 +0100

    fix

--- app/routes/pusher/channels-event.tsx
+++ app/routes/pusher/channels-event.tsx
@@ -7,14 +7,15 @@ export const loader: LoaderFunction = async () => {
   return redirect('/');
 };
 
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: process.env.CLUSTER,
+});
+
 export const action: ActionFunction = async ({ request }) => {
   dotenv.config();
-  const pusher = new Pusher({
-    appId: process.env.APP_ID,
-    key: process.env.KEY,
-    secret: process.env.SECRET,
-    cluster: process.env.CLUSTER,
-  });
 
   const requestText = await request.text();
   const form = new URLSearchParams(requestText);
